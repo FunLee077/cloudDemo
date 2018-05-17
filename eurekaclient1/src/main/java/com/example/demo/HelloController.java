14621d8d9b845728172886c5775410db4bd1e91b
hy <mrlee077@163.com>
2018-05-17 20:37:02 +0800

    初始导入

---
+++ eurekaclient1/src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by hy on 2018/5/17.
+ */
+@RestController
+public class HelloController {
+    @Value("${server.port}")
+    String port;
+    @RequestMapping("/hello")
+    public String home(@RequestParam String name) {
+        return "hi "+name+",i am from port:" +port;
+    }
+}
