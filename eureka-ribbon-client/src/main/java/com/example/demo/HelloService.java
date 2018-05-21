fdb298552a30d0cb19e491b04e570b81ab597d5c
hy <mrlee077@163.com>
2018-05-21 16:34:20 +0800

    ribbon整合熔断器

--- eureka-ribbon-client/src/main/java/com/example/demo/HelloService.java
+++ eureka-ribbon-client/src/main/java/com/example/demo/HelloService.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,7 +14,13 @@ public class HelloService {
 
     @Autowired
     private RestTemplate restTemplate;
+
+@HystrixCommand(fallbackMethod="fallbackError")
     public String hello(String name){
        return restTemplate.getForObject("http://service-hello/hello?name="+name,String.class);
+    }
+
+    public String fallbackError(String name){
+        return "hi"+name+",error!--from robbin & Hystrrix";
     }
 }
